f3b471e3ba99bd734ae2188bd35632ac6866ae8f
Steph Kincarr <steph@polyhydral.co.uk>
2025-10-27 15:35:35 +0000

    add Initial form via local storage

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
+"use client";
+import CommentsPage from "@/app/comments/page.js";
+import React, { useState } from "react";
+
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <CommentsPage />
     </div>
   );
 }
