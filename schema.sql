37cdf82d4dc8b40fe225e2f23a838f01504e5cc3
Corinna <x.corinna.x@hotmail.co.uk>
2025-10-27 14:54:18 +0000

    changes to schema, and created a title

--- schema.sql
+++ schema.sql
@@ -1,17 +1,17 @@
-CREATE TABLE IF NOT EXISTS comments (
-  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  name TEXT NOT NULL,
-  comment TEXT NOT NULL,
-  users_id INTEGER REFERENCES users(id)
-);
-
 CREATE TABLE IF NOT EXISTS users (
-  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  id TEXT PRIMARY KEY,
   username TEXT NOT NULL,
   name TEXT NOT NULL,
   position varchar(255),
   avatar TEXT,
   email TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS comments (
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name TEXT NOT NULL,
+  comment TEXT NOT NULL,
+  users_id TEXT REFERENCES users(id)
 );
 
 CREATE TABLE IF NOT EXISTS feedback (
@@ -22,5 +22,5 @@ CREATE TABLE IF NOT EXISTS feedback (
   do_better TEXT,
   other TEXT,
   useful boolean,
-  users_id INTEGER REFERENCES users(id)
+  users_id TEXT REFERENCES users(id)
 );
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Welcome to H.E.L.I</h1>
+      <h2>Helping Everyone Learn Interactively</h2>
     </div>
   );
 }
