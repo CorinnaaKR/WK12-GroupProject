593e6831b9a2043002b46e894249fa30ab1f7016
Callum James Lodge <callumjlodge@gmail.com>
2025-10-27 11:25:15 +0000

    implementation of clerk with custom signup/signin pages(all routes protected other than /, signup and signin)

--- src/app/sign-in/[[...sign-in]]/page.js
+++ src/app/sign-in/[[...sign-in]]/page.js
@@ -1,7 +1,10 @@
+import { SignIn } from "@clerk/nextjs";
+
 export default function SignInPage() {
   return (
     <>
       <h1>Sign In</h1>
+      <SignIn />
     </>
   );
 }
--- src/app/sign-up/[[...sign-up]]/page.js
+++ src/app/sign-up/[[...sign-up]]/page.js
@@ -1,7 +1,10 @@
+import { SignUp } from "@clerk/nextjs";
+
 export default function SignUpPage() {
   return (
     <>
       <h1>Sign Up</h1>
+      <SignUp />
     </>
   );
 }
