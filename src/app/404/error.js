a5666be1bb3acc810eba30dd65209b632b05ff8b
Corinna <x.corinna.x@hotmail.co.uk>
2025-10-28 14:21:35 +0000

    created the error page and updated readme

--- src/app/404/error.js
+++ src/app/404/error.js
@@ -1,7 +1,13 @@
-export default function ErrorPage() {
+"use client";
+import Link from "next/link";
+
+export default function ErrorPage({ error }) {
   return (
     <>
-      <h1>Error</h1>
+      <h1>Oopsies. Error! </h1>
+      <h2>Back the way you came, you little rebel ⚠️</h2>
+      <p>{error.message}</p>
+      <Link href={"/"}>Go home</Link>
     </>
   );
 }
