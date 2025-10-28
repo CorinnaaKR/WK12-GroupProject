1c1cd48389289f0e82a039921b3eede3a678150a
Callum James Lodge <callumjlodge@gmail.com>
2025-10-28 10:29:24 +0000

    added conditional to page to ensure user is logged in

--- src/components/Temp.jsx
+++ src/components/Temp.jsx
@@ -1,14 +1,2 @@
 import { db } from "@/utils/dbConnection.js";
-export default function Temp({ currUser }) {
-  if (currUser != undefined) {
-    let res = db.query(
-      `INSERT INTO users (id,first_name,last_name,email) VALUES ($1,$2,$3,$4)`,
-      [
-        currUser.id,
-        currUser.firstName,
-        currUser.lastName,
-        currUser.emailAddresses[0].emailAddress,
-      ]
-    );
-  }
-}
+export default function Temp({ currUser }) {}
