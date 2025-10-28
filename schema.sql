d4e5044fb07069cb859cff766c981598dc2b619e
Corinna <x.corinna.x@hotmail.co.uk>
2025-10-28 17:08:10 +0000

    updating readme with today's progress

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS users (
   id TEXT PRIMARY KEY,
   username TEXT NOT NULL,
-  name TEXT NOT NULL,
-  position varchar(255),
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
   avatar TEXT,
   email TEXT NOT NULL
 );
@@ -10,6 +10,7 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS comments (
   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name TEXT NOT NULL,
+  email TEXT NOT NULL,
   comment TEXT NOT NULL,
   users_id TEXT REFERENCES users(id)
 );
@@ -24,3 +25,12 @@ CREATE TABLE IF NOT EXISTS feedback (
   useful boolean,
   users_id TEXT REFERENCES users(id)
 );
+
+
+SELECT comments.name, comments.email, comments.comment, comments.users_id
+FROM comments
+JOIN users ON users_id = comments.users_id; 
+
+SELECT feedback.name, feedback.email, feedback.done_well, feedback.do_better, feedback.other, feedback.useful, feedback.users_id
+FROM feedback
+JOIN users ON users_id = feedback.users_id; 
