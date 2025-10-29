bf8920bff3bb0b708d7a6a9f743bc828abe0f7bf
Corinna <x.corinna.x@hotmail.co.uk>
2025-10-29 17:01:52 +0000

    adding to resources page and updating readme

--- src/app/resources/page.js
+++ src/app/resources/page.js
@@ -2,6 +2,7 @@ export default function ResourcePage() {
   return (
     <div className="resources">
       <h1>Resources</h1>
+
       <h2>National Police Prevent</h2>
       <ul>
         <li>
@@ -9,61 +10,69 @@ export default function ResourcePage() {
         </li>
         <li>Prevent advice line: 0800 011 3764, or in an emergency 999</li>
       </ul>
-<div className="east">
-      <h3>East of England</h3>
-      <ul>
-        <p className="norfolk">Norfolk Police Prevent team</p>
-        <li>01953 423905 or 01953 423896</li>
-        <li>prevent@norfolk.police.uk</li>
+      <div className="east">
+        <h3>East of England</h3>
+        <ul>
+          <p className="norfolk">Norfolk Police Prevent team</p>
+          <li>01953 423905 or 01953 423896</li>
+          <li>prevent@norfolk.police.uk</li>
+          <li>
+            {" "}
+            Make a referral online at{" "}
+            <a href="https://www.norfolk.police.uk/advice/advice-and-information/t/prevent/prevent/beta/prevent-team-referral" />
+          </li>
+        </ul>
+
+        <p className="suffolk">Suffolk Police Prevent team</p>
+        <ul>
+          <li>01473 613500</li>
+          <li>prevent@norfolk.police.uk</li>
+          <li>
+            Make a referral online at{" "}
+            <a href="https://www.suffolk.police.uk/advice/advice-and-information/t/prevent/prevent/beta/prevent-team-referral/"></a>
+          </li>
+        </ul>
+
+        <p className="cambridge">Cambridgeshire & Peterborough Prevent Team</p>
+        <li>01480 422596</li>
+        <li>prevent@cambs.pnn.police.uk</li>
         <li>
-          {" "}
           Make a referral online at{" "}
-          <a href="https://www.norfolk.police.uk/advice/advice-and-information/t/prevent/prevent/beta/prevent-team-referral" />
+          <a href="https://www.cambs.police.uk/advice/advice-and-information/t/prevent/prevent/beta/prevent-team-referral/"></a>
         </li>
-      </ul>
-
-      <p className="suffolk">Suffolk Police Prevent team</p>
-      <ul>
-        <li>01473 613500</li>
-        <li>prevent@norfolk.police.uk</li>
-        <li>
-          Make a referral online at{" "}
-          <a href="https://www.suffolk.police.uk/advice/advice-and-information/t/prevent/prevent/beta/prevent-team-referral/"></a>
-        </li>
-      </ul>
-
-<p className="cambridge">Cambridgeshire & Peterborough Prevent Team</p>
-      <li>01480 422596</li>
-      <li>prevent@cambs.pnn.police.uk</li>
-      <li>
-        Make a referral online at{" "}
-        <a href="https://www.cambs.police.uk/advice/advice-and-information/t/prevent/prevent/beta/prevent-team-referral/"></a>
-      </li>
-</div>
-
-<div className="midlands">
-    <h4>East Midlands</h4> 
-      <p className="derby">Derbyshire Prevent Team</p>
-      <li>0300 122 8694</li>
-      <li>
-        Make a referral online at{" "}
-        <a href="https://www.derbyshire.police.uk/advice/advice-and-information/t/prevent/prevent/beta/prevent-team-referral/"></a>
-      </li>
-
-      <p className="notts">Nottinghamshire Prevent Team</p>
-      <li>0800 011 3764</li>
-      <li>prevent@notts.police.uk</li>
-      <li>
-        <a href="Make a referral online at https://www.nottinghamshire.police.uk/advice/advice-and-information/t/prevent/prevent/beta/prevent-team-referral/"></a>
-      </li>
       </div>
 
-      <p className="Leicester">Leicester Prevent Team</p>
-      <ul>
-        <li>Call 101 & enter extension 6770 Make a referral online at
-      https://www.leics.police.uk/advice/advice-and-information/t/prevent/prevent/beta/prevent-team-referral/
-      </li>
-      <h10>Humberside Prevent Team</h10>
+      <div className="midlands">
+        <h4>East Midlands</h4>
+        <p className="derby">Derbyshire Prevent Team</p>
+        <ul>
+          <li>0300 122 8694</li>
+          <li>
+            Make a referral online at{" "}
+            <a href="https://www.derbyshire.police.uk/advice/advice-and-information/t/prevent/prevent/beta/prevent-team-referral/"></a>
+          </li>
+        </ul>
+
+        <p className="notts">Nottinghamshire Prevent Team</p>
+        <ul>
+          <li>0800 011 3764</li>
+          <li>prevent@notts.police.uk</li>
+          <li>
+            <a href="Make a referral online at https://www.nottinghamshire.police.uk/advice/advice-and-information/t/prevent/prevent/beta/prevent-team-referral/"></a>
+          </li>
+        </ul>
+
+        <p className="Leicester">Leicester Prevent Team</p>
+        <ul>
+          <li>
+            Call 101 & enter extension 6770 Make a referral online at{" "}
+            <a href="https://www.leics.police.uk/advice/advice-and-information/t/prevent/prevent/beta/prevent-team-referral/"></a>
+          </li>
+        </ul>
+      </div>
+
+      <h5>Yorkshire & the Humber</h5>
+      <p className="humber">Humberside Prevent Team</p>
       <ul>
         <li>01482 356413</li>
         <li>
@@ -72,10 +81,12 @@ export default function ResourcePage() {
         </li>
       </ul>
     </div>
-  
+  );
+}
 
-<div className="northWest">
-<h5>North West</h5>
+{
+  /* <div className="Liv"
+
  <p className="liverpool">Liverpool Police Prevent Team</p>
  <ul>
       <li>0151 777 8506</li>
@@ -83,8 +94,5 @@ export default function ResourcePage() {
       <li>
         Make a referral online at{" "}
         <a href="https://www.merseyside.police.uk/advice/advice-and-information/t/prevent/prevent/alpha/prevent-referral/"></a>
-      </li>
-</ul>
-</div>
-);
+      </li>  */
 }
